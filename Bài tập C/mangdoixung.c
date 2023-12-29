05b82b5d3df48092102a47711e59b49d4f86829f
Dang Thong Nghia <141739914+dangthongnghia@users.noreply.github.com>
2023-12-29 10:04:14 +0700

    Update mangdoixung.c

--- Bài tập C/mangdoixung.c
+++ Bài tập C/mangdoixung.c
@@ -1,52 +1,37 @@
-#include <stdio.h>
-#define MAX_SIZE 100 // Gi?i h?n kích thu?c m?ng
+#include<stdio.h>
 
-int main()
-{
-    int arr[MAX_SIZE]; // Khai báo m?ng
-    int size; // Khai báo bi?n luu kích thu?c m?ng
-    int i, j; // Khai báo bi?n luu ch? s? m?ng
-    int isSymmetric = 1; // Khai báo bi?n c? ki?m tra d?i x?ng
-
-    // Nh?p kích thu?c m?ng t? bàn phím
-    printf("Nh?p kích thu?c m?ng: ");
-    scanf("%d", &size);
-
-    // Nh?p các ph?n t? m?ng t? bàn phím
-    printf("Nh?p các ph?n t? m?ng: ");
-    for(i=0; i<size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    // Ki?m tra m?ng d?i x?ng
-    // Gán j b?ng ch? s? ph?n t? cu?i cùng c?a m?ng
-    j = size - 1;
-
-    // L?p qua n?a d?u m?ng
-    for(i=0; i<size/2; i++)
-    {
-        // N?u ph?n t? ? v? trí i khác ph?n t? ? v? trí j
-        if(arr[i] != arr[j])
-        {
-            // Gán c? b?ng 0 và d?ng vòng l?p
-            isSymmetric = 0;
-            break;
-        }
-
-        // Gi?m j di 1 don v?
-        j--;
-    }
-
-    // In k?t qu?
-    if(isSymmetric == 1)
-    {
-        printf("M?ng d?i x?ng.\n");
-    }
-    else
-    {
-        printf("M?ng không d?i x?ng.\n");
-    }
-
-    return 0;
+int main() {
+   int arr[]; // khai bÃ¡o máº£ng
+   int size; // kÃ­ch thÆ°á»›c máº£ng
+   int i, N; // Biáº¿n lÆ°u chá»‰ sá»‘ máº£ng
+   int kiemtra = 0; //  kiá»ƒm tra Ä‘á»‘i xá»©ng
+   
+  printf("KÃ­ch thÆ°á»›c máº£ng:", size);
+  scanf("%d", &size);
+  
+  printf("Nháº­p cÃ¡c pháº§n tá»­ máº£ng: ", arr[i]);
+  for(i=0; i<size; i++) {
+      scanf("%d", &arr[i]); // Nháº­p cÃ¡c pháº§n tá»­ máº£ng 
+      N = size - 1;
+      
+      for(i=0; i< size/2; i++) // Láº·p qua ná»­a Ä‘áº§u máº£ng
+      {
+          if(arr[i] != arr[N]);{
+          kiemtra=0;
+          
+          break
+          }
+          N--
+          
+      }
+      if(kiemtra == 1)
+      { 
+          printf("Máº£ng Ä‘á»‘i xá»©ng.\n");
+      } else {
+          printf("Máº£ng khÃ´ng Ä‘á»‘i xá»©ng. \n");
+          
+      }
+        
+    
+  return 0;
 }
